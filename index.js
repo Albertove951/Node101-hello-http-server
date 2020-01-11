ab00fd4622895b9312853774fa9d22ccca3b123f
Alberto Vega <albertovega00@gmail.com>
2020-01-11 14:32:35 -0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// write your code here
+var http = require('http');
+
+const server = http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+})
+    server.listen(8080) 
+    console.log('Server is listening on localhost:8080')
